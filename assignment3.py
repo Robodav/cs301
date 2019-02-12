c6c382ef56724ae9cc3de71fdec320b1c1796108
Robodav <dvandiver64@gmail.com>
2019-02-12 16:46:11 -0700

    Add queue and deque methods

--- assignment3.py
+++ assignment3.py
@@ -25,15 +25,72 @@ class Stack:
 	def size(self):
 		return len(self.stack)
 
-testStack = Stack()
+# testStack = Stack()
 
-testStack.push(1)
-testStack.push(2)
-testStack.push(3)
+# testStack.push(1)
+# testStack.push(2)
+# testStack.push(3)
 
-print(testStack.peek())
-print(testStack.pop())
-print(testStack.size())
-print(testStack.pop())
-print(testStack.pop())
-print(testStack.isEmpty())
+# print(testStack.peek())
+# print(testStack.pop())
+# print(testStack.size())
+# print(testStack.pop())
+# print(testStack.pop())
+# print(testStack.isEmpty())
+
+class Queue:
+	def __init__(self):
+		self.queue = []
+
+	def enqueue(self, item):
+		self.queue.insert(0,item)
+
+	def dequeue(self):
+		return self.queue.pop(0)
+
+	def isEmpty(self):
+		return (len(self.queue) == 0)
+
+	def size(self):
+		return len(self.queue)
+
+# testQueue = Queue()
+
+# testQueue.enqueue(1)
+# testQueue.enqueue(2)
+# testQueue.enqueue(3)
+
+# print(testQueue.dequeue())
+# print(testQueue.dequeue())
+# print(testQueue.dequeue())
+
+# print(testQueue.isEmpty())
+
+class Deque:
+	def __init__(self):
+		self.deque = []
+
+	def addFront(self, item):
+		self.deque.insert(0,item)
+
+	def addRear(self, item):
+		self.deque.append(item)
+
+	def removeFront(self):
+		return self.deque.pop(0)
+
+	def removeRear(self):
+		return self.deque.pop()
+
+	def isEmpty(self):
+		return len(self.deque) == 0
+
+	def size(self):
+		return len(self.deque)
+
+# testDeque = Deque()
+
+# testDeque.addFront(1)
+# testDeque.addRear(1)
+# testDeque.removeFront()
+# print(testDeque.size())
