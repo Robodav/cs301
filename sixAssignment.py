36caed52c9f14b44daa265c8423595555d71f211
Robodav <dvandiver64@gmail.com>
2019-04-09 15:58:04 -0600

    Begin current node process

--- sixAssignment.py
+++ sixAssignment.py
@@ -29,18 +29,25 @@ class Tree:
 
     def __repr__(self):
         visual = ""
-        visual += str(self.root.getData())
-        nodes = self.root.getChildren()
+        nodes = [self.root]
         while nodes != []:
             visual += "\n"
             for node in nodes:
                 visual += str(node.getData())
-            nodes = []
-            
+                nodes.remove(node)
+                for n in node.getChildren():
+                    nodes.append(n)
         return visual
 
     def getCurrentNode(self):
-        return self.currentNode
+        current = "Current Node:\n"
+        current += str(self.currentNode.getData()) + "\n"
+        current += "Children:\n"
+        current += "["
+        for n in self.currentNode.getChildren():
+            current += n.getData()
+        current += "]"
+        return current
     
     def setCurrentNode(self):
         return
@@ -65,12 +72,14 @@ class Tree:
         return self.root == None
 
 def main():
-    sampleroot = Node(24)
+    # sampleroot = Node(24)
     # print(sampleroot)
     tree1 = Tree()
-    tree1.add(sampleroot)
-    print(tree1)
-    # tree1.print()
+    tree1.add(24)
+    tree1.add(13)
+    tree1.add(5)
+    tree1.getCurrentNode()
+    # print(tree1)
 
 if __name__ == "__main__":
     main()
